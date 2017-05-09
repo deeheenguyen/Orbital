719d42c29dc63259f5ae1f36bcb39d3af951ce7a
NguyenVanHoang <nguyenvanhoang7398@gmail.com>
2017-05-09 20:08:46 +0800

    Setup basic react sever

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Orbital
+Install necessary dependencies:
+    npm install
+Run server in local host:
+    npm start
---
+++ client/nuswhere.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import css from './styles/style.styl';
+
+render(<p>Hello Orbital</p>, document.getElementById('root'));
