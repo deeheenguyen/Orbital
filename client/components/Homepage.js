dfb26889329be803e9de40d4dbdd4a70203a41a3
NguyenVanHoang <nguyenvanhoang7398@gmail.com>
2017-05-28 22:17:33 +0700

    Refract homepage with css and form input

--- client/components/Homepage.js
+++ client/components/Homepage.js
@@ -1,25 +1,29 @@
-import { Link } from 'react-router';
+import { Link, Router } from 'react-router';
 import PhotoGrid from './PhotoGrid.js';
 import React from 'react';
 
-var textStyle = {
-    textAlign: 'center',
-    fontWeight: 'bold',
-    fontSize: 50,
-    marginTop: 0
-}
 class Homepage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  static get contextTypes() {
+    return {
+      router: React.PropTypes.object.isRequired,
+    };
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log("change route")
+    this.context.router.push('/results');
+  }
   render() {
     return (
-      <div style ={textStyle}>
-      <form>
-          <label>
-              Name:
-                <input type="text" name="name" />
-          </label>
-
-             <li><Link to="/results">SEARCH</Link></li>
-      </form>
+      <div className="homepage">
+        <form onSubmit={this.handleSubmit}>
+            <input type="text" ref="search-key" /><br/>
+            <input type="submit" className="button" value="SEARCH"/>
+        </form>
       </div>
     );
   }
